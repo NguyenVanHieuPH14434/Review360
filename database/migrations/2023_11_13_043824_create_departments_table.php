d69f8854e73e4fda6fb5c06064e4d540741e0846
HoangND <hoangnguyenit98@gmail.com>
2023-11-14 11:54:07 +0700

    init base

---
+++ database/migrations/2023_11_13_043824_create_departments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code', 10)->comment('auto generate start DT0001');;
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->integer('status')->default(1)->comment('0-inactive, 1-active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};
