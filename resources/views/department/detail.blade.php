91c5a4f48d655cb0b3f069ee77256dc0534cc3b5
hieu <nguyenvanhieugl2001@gmail.com>
2023-11-23 23:46:42 +0700

    update jobtitle, department

--- resources/views/department/detail.blade.php
+++ resources/views/department/detail.blade.php
@@ -12,10 +12,15 @@
                 <div class="card">
                     <x-card-title title="Thông tin phòng ban">
                         <x-slot:create>
-                            <a href="{{ route('department.create') }}" class="btn mb-1 waves-effect waves-light btn-primary">
+                            <a href="{{ route('department.create') }}" class="btn mb-1 waves-effect waves-light btn-success">
                                 <i class="ti ti-plus"></i> Tạo mới
                             </a>
                         </x-slot:create>  
+                        <x-slot:update>
+                            <a href="{{ route('department.edit', $department->id) }}" class="btn mb-1 waves-effect waves-light btn-secondary">
+                                <i class="ti ti-edit fs-5"></i> Cập nhật
+                            </a>
+                        </x-slot:update>  
                     </x-card-title>
                     <form class="form-horizontal">
                         <div class="form-body">
@@ -67,36 +72,21 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group row">
-                                            <label class="control-label text-end col-md-3">Trạng thái:</label>
+                                            <label class="control-label text-end col-md-3">Mô tả:</label>
                                             <div class="col-md-9">
                                                 <p class="form-control-static">
-                                                    {{ $department->status !== 2 ? 'Hoạt động' : 'Không hoạt động' }}</p>
+                                                    {{ $department->description }}</p>
                                             </div>
                                         </div>
                                     </div>
-
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title mb-0">Mô tả: </h5>
-                                <span class="form-control-static">{{ $department->description }}</span>
-                            </div>
-                            <hr>
-                            <div class="form-actions">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="row">
-                                                <div class="col-md-offset-3 col-md-9">
-                                                    <a href="{{ route('department.edit', $department->id) }}"
-                                                        class="btn btn-primary">
-                                                        <i class="ti ti-edit fs-5"></i>
-                                                        Cập nhật
-                                                    </a>
-                                                </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group row">
+                                            <label class="control-label text-end col-md-3">Trạng thái:</label>
+                                            <div class="col-md-9">
+                                                <p class="form-control-static mb-1 badge rounded-pill text-bg-{{ $department->status !== 2 ? "success" : "danger" }} fw-semibold fs-2">
+                                                    {{ $department->status !== 2 ? 'Hoạt động' : 'Không hoạt động' }}</p>
                                             </div>
                                         </div>
-                                        <div class="col-md-6"></div>
                                     </div>
                                 </div>
                             </div>
--- resources/views/jobTitle/detail.blade.php
+++ resources/views/jobTitle/detail.blade.php
@@ -12,10 +12,17 @@
                 <div class="card">
                     <x-card-title title="Thông tin chức danh">
                         <x-slot:create>
-                            <a href="{{ route('jobTitle.create') }}" class="btn mb-1 waves-effect waves-light btn-primary">
+                            <a href="{{ route('jobTitle.create') }}" class="btn mb-1 waves-effect waves-light btn-success">
                                 <i class="ti ti-plus"></i> Tạo mới
                             </a>
                         </x-slot:create>  
+                        <x-slot:update>
+                            <a href="{{ route('jobTitle.edit', $jobTitle->id) }}"
+                                class="btn mb-1 waves-effect waves-light btn-secondary">
+                                <i class="ti ti-edit fs-5"></i>
+                                Cập nhật
+                            </a>
+                        </x-slot:update>  
                     </x-card-title>
                     <form class="form-horizontal">
                         <div class="form-body">
@@ -67,36 +74,21 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group row">
-                                            <label class="control-label text-end col-md-3">Trạng thái:</label>
+                                            <label class="control-label text-end col-md-3">Mô tả:</label>
                                             <div class="col-md-9">
                                                 <p class="form-control-static">
-                                                    {{ $jobTitle->status !== 2 ? 'Hoạt động' : 'Không hoạt động' }}</p>
+                                                    {{ $jobTitle->description }}</p>
                                             </div>
                                         </div>
                                     </div>
-
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title mb-0">Mô tả: </h5>
-                                <span class="form-control-static">{{ $jobTitle->description }}</span>
-                            </div>
-                            <hr>
-                            <div class="form-actions">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="row">
-                                                <div class="col-md-offset-3 col-md-9">
-                                                    <a href="{{ route('jobTitle.edit', $jobTitle->id) }}"
-                                                        class="btn btn-primary">
-                                                        <i class="ti ti-edit fs-5"></i>
-                                                        Cập nhật
-                                                    </a>
-                                                </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group row">
+                                            <label class="control-label text-end col-md-3">Trạng thái:</label>
+                                            <div class="col-md-9">
+                                                <p class="form-control-static mb-1 badge rounded-pill text-bg-{{ $jobTitle->status !== 2 ? "success" : "danger" }} fw-semibold fs-2">
+                                                    {{ $jobTitle->status !== 2 ? 'Hoạt động' : 'Không hoạt động' }}</p>
                                             </div>
                                         </div>
-                                        <div class="col-md-6"></div>
                                     </div>
                                 </div>
                             </div>
